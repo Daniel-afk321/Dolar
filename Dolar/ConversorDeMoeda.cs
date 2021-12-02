e62eebecfa72acf4351dc430a92ca4bf900b7c48
Daniel-afk321 <83432335+Daniel-afk321@users.noreply.github.com>
2021-12-01 21:49:50 -0300

    Adicionar arquivos de projeto.

---
+++ Dolar/ConversorDeMoeda.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dolar
+{
+    class ConversorDeMoeda
+    {
+        //Static pode ser aplicado a variáveis ​​e métodos, tornando-os métodos / variáveis ​
+        public static double Iof = 2.95;
+        public static double reais;
+
+        //função que retorna a Conversão Do Dolar
+        public static double ConversaoDolar(double cotacao, double valorCompra)
+        {
+
+            double total = valorCompra + (valorCompra * (Iof / 100.0));
+            return total *= cotacao;
+  
+        }
+        //Função que retorna o valor em reais sem a % do IOF
+        public static double  ValorEmReais(double cotacao, double valorCompra)
+        {
+
+            reais = cotacao * valorCompra;
+            return reais;
+            
+        }
+
+    }
+}
---
+++ Dolar/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+
+namespace Dolar
+{
+    class Program
+    {
+       
+       
+
+        static void Main(string[] args)
+        {
+            // IOF 4,08 = link do site que peguei a informação 
+            //https://agenciabrasil.ebc.com.br/economia/noticia/2021-09/comecam-a-valer-hoje-novas-aliquotas-do-IOF
+            //Cotação atual R$5,607, https://economia.uol.com.br/cotacoes/cambio/
+            //=====================================================Programa==========================================================
+            string cont = "S";
+            //Caso o usuario coloque SIM ou S, executa as linhas declaradas dentro do while; do contrário, sai do loop
+            while (cont == "S" || cont == "SIM")
+
+            {
+                // o usuario vai entrar com a cotação do dolar
+                Console.WriteLine("Digite a cotação atual do dolar?");
+                double cotacao = double.Parse(Console.ReadLine());
+                //o usuario vai entrar com um valor para comprar dolares 
+                Console.WriteLine("Digite quantos dolares voce ira comprar?");
+                double compra = double.Parse(Console.ReadLine());
+                double valorCompraComIof = ConversorDeMoeda.ConversaoDolar(cotacao, compra);
+                double Reais = ConversorDeMoeda.ValorEmReais(cotacao, compra);
+
+                //Valor que ele ira pagar em Reais 
+                Console.WriteLine($"Valor a ser pago em Reais = R${Reais.ToString("C0")}");
+                //informa a % paga de IOF pelo usuário
+                Console.WriteLine("valor a ser pago de IOF = {0}%", ConversorDeMoeda.Iof );
+                //valor total da compra com o IOF incluido
+                Console.WriteLine($"Valor total da compra com IOF = R${valorCompraComIof.ToString("C0")}");
+                //esta perguntando se o usuario que fazer um novo calculo
+                Console.WriteLine("Deseja calcular um novo caso?(Sim/Não)");
+                //ToUpper é usado para converter qualquer caracter em letras maiúsculas para nao da poblema no programa
+                cont = Console.ReadLine().ToUpper();
+
+            }            
+
+        }
+    }
+}
